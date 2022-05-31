a055e35648075ffeca5aa965d7413b3441ed361f
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:22:23 -0300

    Update 3.sql

--- Consultas medias/3.sql
+++ Consultas medias/3.sql
@@ -7,21 +7,21 @@ FROM
 	film_actor fa
 JOIN
 	film f
-    USING(film_id)
+    	USING(film_id)
 GROUP BY
 	fa.film_id
 HAVING
 	total_actors
-    =	(	# Mayor cantidad de actores en una película.
-			SELECT
-				MAX(total_actors)
-			FROM
-				(	# Número de actores en cada película.
-					SELECT
-						COUNT(fa.actor_id) AS total_actors
-					FROM
-						film_actor fa
-					GROUP BY
-						fa.film_id
-				) fta
-		)
+	= (	# Mayor cantidad de actores en una película.
+		SELECT
+			MAX(total_actors)
+		FROM
+			(	# Número de actores en cada película.
+				SELECT
+					COUNT(fa.actor_id) AS total_actors
+				FROM
+					film_actor fa
+				GROUP BY
+					fa.film_id
+			) fta
+	  )
