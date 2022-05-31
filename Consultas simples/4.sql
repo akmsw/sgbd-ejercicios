d9a388bd57f22b0762183e7e5c382e0448583f7a
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:18:54 -0300

    Update 4.sql

--- Consultas simples/4.sql
+++ Consultas simples/4.sql
@@ -2,32 +2,32 @@
 
 SELECT
 	c.first_name,
-    c.last_name,
-    a.address AS customer_address,
-    a.district,
-    ci.city,
-    co.country,
-    COUNT(r.rental_id) AS total_rentals,
-    SUM(p.amount) AS total_payments
+    	c.last_name,
+    	a.address AS customer_address,
+    	a.district,
+    	ci.city,
+    	co.country,
+	COUNT(r.rental_id) AS total_rentals,
+    	SUM(p.amount) AS total_payments
 FROM
 	customer c
 JOIN
 	address a
-    USING(address_id)
+    	USING(address_id)
 JOIN
 	city ci
-    USING(city_id)
+    	USING(city_id)
 JOIN
 	country co
-    USING(country_id)
+    	USING(country_id)
 JOIN
 	rental r
-    USING(customer_id)
+    	USING(customer_id)
 JOIN
 	payment p
-    USING(rental_id)
+    	USING(rental_id)
 GROUP BY
 	r.customer_id
 ORDER BY
 	ci.city,
-    co.country
+    	co.country
