ea5443a9d1a63533e36ae73b29df68a3d624948c
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:27:06 -0300

    Update 5.sql

--- Consultas medias/5.sql
+++ Consultas medias/5.sql
@@ -2,32 +2,32 @@
 
 SELECT
 	a.first_name,
-    a.last_name
+    	a.last_name
 FROM
 	actor a
 WHERE
 	a.actor_id
-    IN
-    	(
-            SELECT
-                DISTINCT fa.actor_id
-            FROM
-                film_actor fa
-            WHERE
-                fa.film_id
-                IN
-                    (
-                        SELECT
-                            f.film_id
-                        FROM
-                            film f
-                        WHERE
-                            f.length
-                            =	(
-                                    SELECT
-                                        MAX(length)
-                                    FROM
-                                        film
-                                )
-                    )
+    	IN
+	(
+		SELECT
+			DISTINCT fa.actor_id
+		FROM
+			film_actor fa
+		WHERE
+			fa.film_id
+		IN
+		(
+			SELECT
+				f.film_id
+			FROM
+				film f
+			WHERE
+				f.length
+				=	(
+						SELECT
+							MAX(length)
+						FROM
+							film
+					)
 		)
+	)
