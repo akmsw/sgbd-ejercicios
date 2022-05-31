4d3ff6261a2599256e8e7b1ee0ff432300fedcfd
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:29:59 -0300

    Update 8.sql

--- Consultas medias/8.sql
+++ Consultas medias/8.sql
@@ -2,28 +2,28 @@
 
 SELECT
 	a.first_name,
-    a.last_name
+    	a.last_name
 FROM
 	actor a
 JOIN
 	(
-        SELECT
-            fa.actor_id,
-            COUNT(DISTINCT fc.category_id) AS total_categories
-        FROM
-            film_actor fa
-        JOIN
-            film_category fc
-            USING(film_id)
-        GROUP BY
-            fa.actor_id
-        HAVING
-            total_categories
-            =	(
-                    SELECT
-                        COUNT(*) - 2
-                    FROM
-                        category
-                )
+		SELECT
+			fa.actor_id,
+			    COUNT(DISTINCT fc.category_id) AS total_categories
+		FROM
+			    film_actor fa
+		JOIN
+			    film_category fc
+			    USING(film_id)
+		GROUP BY
+			    fa.actor_id
+		HAVING
+			    total_categories
+			    =	(
+					SELECT
+						COUNT(*) - 2
+				    	FROM
+						category
+				)
 	) atc
-    USING(actor_id)
+	USING(actor_id)
