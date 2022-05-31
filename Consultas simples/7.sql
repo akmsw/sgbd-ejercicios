0c761b48cbce32b5d66253a4899d289fc3ee3ed3
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:20:11 -0300

    Update 7.sql

--- Consultas simples/7.sql
+++ Consultas simples/7.sql
@@ -2,30 +2,30 @@
 
 SELECT
 	c.first_name,
-    c.last_name,
-    a.address AS customer_address,
-    a.district,
-    ci.city,
-    co.country,
-    COUNT(r.rental_id) AS total_rentals,
-    SUM(p.amount) AS total_payments
+    	c.last_name,
+    	a.address AS customer_address,
+    	a.district,
+    	ci.city,
+    	co.country,
+    	COUNT(r.rental_id) AS total_rentals,
+    	SUM(p.amount) AS total_payments
 FROM
 	customer c
 JOIN
 	address a
-    USING(address_id)
+    	USING(address_id)
 JOIN
 	city ci
-    USING(city_id)
+    	USING(city_id)
 JOIN
 	country co
-    USING(country_id)
+    	USING(country_id)
 JOIN
 	rental r
-    USING(customer_id)
+    	USING(customer_id)
 JOIN
 	payment p
-    USING(rental_id)
+    	USING(rental_id)
 WHERE
 	MONTH(r.rental_date) = 5
 GROUP BY
@@ -34,6 +34,6 @@ HAVING
 	total_payments > 10
 ORDER BY
 	total_payments
-    DESC
+    	DESC
 LIMIT
 	10
