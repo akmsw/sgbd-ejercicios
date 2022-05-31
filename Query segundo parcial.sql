4b609c029ae8bb69e43b5e06d9e4db62a921b1ea
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:37:30 -0300

    Update Query segundo parcial.sql

--- Query segundo parcial.sql
+++ Query segundo parcial.sql
@@ -10,90 +10,88 @@ De cada película, en el listado se deberá indicar:
 
 SELECT
 	f.title AS movie_title,
-    COUNT(fa.actor_id) AS total_actors,
-    movie_rentals,
-    (	/*Promedio de copias de films*/
-        SELECT
-            FLOOR(AVG(film_copies))
-        FROM
-            (	/*Cantidad de copias de cada film.*/
-                SELECT
-                    f.film_id,
-                    COUNT(i.inventory_id) AS film_copies
-                FROM
-                    film f
-                LEFT JOIN
-                    inventory i
-                    USING(film_id)
-                GROUP BY
-                    f.film_id
-            ) cpf
-    ) AS average_copies
+    	COUNT(fa.actor_id) AS total_actors,
+    	movie_rentals,
+    	(	/*Promedio de copias de films*/
+        	SELECT
+            		FLOOR(AVG(film_copies))
+        	FROM
+            		(	/*Cantidad de copias de cada film.*/
+                		SELECT
+                    			f.film_id,
+                    			COUNT(i.inventory_id) AS film_copies
+                		FROM
+                    			film f
+				LEFT JOIN
+                    			inventory i
+                    			USING(film_id)
+                		GROUP BY
+                    			f.film_id
+            		) cpf
+	) AS average_copies
 FROM
 	film_actor fa
 JOIN
 	film f
-    USING(film_id)
+    	USING(film_id)
 JOIN
 	actor a
-    USING(actor_id)
+    	USING(actor_id)
 JOIN
-	(
-        /*Cantidad de alquileres por película.*/
-        SELECT
-        	f.film_id,
+	(	/*Cantidad de alquileres por película.*/
+        	SELECT
+        		f.film_id,
 			COUNT(rental_id) AS movie_rentals
 		FROM
-        	rental r
-        JOIN
-        	inventory i
-        	USING(inventory_id)
-        JOIN
-        	film f
-        	USING(film_id)
-        GROUP BY
-        	f.film_id
+        		rental r
+        	JOIN
+        		inventory i
+        		USING(inventory_id)
+        	JOIN
+			film f
+        		USING(film_id)
+        	GROUP BY
+        		f.film_id
 	) app
-    USING(film_id)
+    	USING(film_id)
 WHERE
 	f.film_id
-    IN
-    	(	/*IDs de films con más copias que el promedio.*/
-            SELECT
-                id_film
-            FROM
-                (	/*Datos de films con más copias que el promedio.*/
-                    SELECT
-                        f.film_id AS id_film,
-                        COUNT(i.inventory_id) AS film_copies
-                    FROM
-                        film f
-                    LEFT JOIN
-                        inventory i
-                        USING(film_id)
-                    GROUP BY
-                        f.film_id
-                    HAVING
-                        film_copies
-                        > (
-                            /*Promedio de copias de films.*/
-                            SELECT
-                                FLOOR(AVG(film_copies))
-                            FROM
-                                (	/*Cantidad de copias de cada film.*/
-                                    SELECT
-                                        f.film_id,
-                                        COUNT(i.inventory_id) AS film_copies
-                                    FROM
-                                        film f
-                                    LEFT JOIN
-                                        inventory i
-                                        USING(film_id)
-                                    GROUP BY
-                                        f.film_id
-                                ) cpf
-                          )
-                ) idf
-		)
+    	IN
+	(	/*IDs de films con más copias que el promedio.*/
+		SELECT
+			id_film
+            	FROM
+                	(	/*Datos de films con más copias que el promedio.*/
+                    		SELECT
+                        		f.film_id AS id_film,
+                        		COUNT(i.inventory_id) AS film_copies
+                    		FROM
+                        		film f
+                    		LEFT JOIN
+                        		inventory i
+					USING(film_id)
+                    		GROUP BY
+                        		f.film_id
+                    		HAVING
+                        		film_copies
+                        		> (	/*Promedio de copias de films.*/
+                            			SELECT
+                                			FLOOR(AVG(film_copies))
+                            			FROM
+                                			(	/*Cantidad de copias de cada film.*/
+                                    				SELECT
+                                        				f.film_id,
+                                        				COUNT(i.inventory_id) AS film_copies
+                                    				FROM
+                                        				film f
+                                    				LEFT JOIN
+                                        				inventory i
+                                        				USING(film_id)
+                                    				GROUP BY
+                                        				f.film_id
+                                			) cpf
+                          		)
+                	) idf
+	)
 GROUP BY
 	fa.film_id
