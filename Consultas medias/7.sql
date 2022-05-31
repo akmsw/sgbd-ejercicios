c9c1377ee9aa679a029363e17b2143aab02799da
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:29:00 -0300

    Update 7.sql

--- Consultas medias/7.sql
+++ Consultas medias/7.sql
@@ -2,29 +2,29 @@
 
 SELECT
 	a.first_name,
-    a.last_name
+    	a.last_name
 FROM
 	actor a
 WHERE
 	a.actor_id
-    IN
+    	IN
     	(
-            SELECT
-                DISTINCT a.actor_id
-            FROM
-                rental r
-            JOIN
-                inventory i
-                USING(inventory_id)
-            JOIN
-                film f
-                USING(film_id)
-            JOIN
-                film_actor fa
-                USING(film_id)
-            JOIN
-                actor a
-                USING(actor_id)
-            WHERE
-                MONTH(r.rental_date) = 5
-		)
+		SELECT
+                	DISTINCT a.actor_id
+            	FROM
+                	rental r
+            	JOIN
+			inventory i
+			USING(inventory_id)
+            	JOIN
+			film f
+			USING(film_id)
+    		JOIN
+			film_actor fa
+			USING(film_id)
+            	JOIN
+			actor a
+			USING(actor_id)
+            	WHERE
+			MONTH(r.rental_date) = 5
+	)
