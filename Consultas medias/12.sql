8b5d7bc54fd27f16557168fc9716059d0d58337d
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:32:14 -0300

    Update 12.sql

--- Consultas medias/12.sql
+++ Consultas medias/12.sql
@@ -2,29 +2,28 @@
 
 SELECT
 	c.first_name,
-    c.last_name,
-    f.title,
-    DATEDIFF(	
-        		r.return_date,
-				DATE_ADD(	
-                    		rental_date,
-        					INTERVAL f.rental_duration DAY
-                    	)
+    	c.last_name,
+    	f.title,
+    	DATEDIFF(	
+			r.return_date,
+			DATE_ADD(	
+					rental_date,
+					INTERVAL f.rental_duration DAY
+                    		)
         	) AS return_delay
 FROM
 	rental r
 JOIN
 	customer c
-    USING(customer_id)
+    	USING(customer_id)
 JOIN
 	inventory i
-    USING(inventory_id)
+    	USING(inventory_id)
 JOIN
 	film f
-    USING(film_id)
+    	USING(film_id)
 WHERE
-	
-    DATE_ADD(	
-        		rental_date,
-        		INTERVAL f.rental_duration DAY
+	DATE_ADD(	
+			rental_date,
+			INTERVAL f.rental_duration DAY
         	) < r.return_date
