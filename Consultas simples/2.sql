eb82bb98c3bd504ba7207ebe1d9bd694f11ed224
akmsw <66426042+akmsw@users.noreply.github.com>
2022-05-31 20:17:58 -0300

    Update 2.sql

--- Consultas simples/2.sql
+++ Consultas simples/2.sql
@@ -2,22 +2,22 @@
 
 SELECT
 	c.first_name,
-    c.last_name,
-    a.address AS customer_address,
-    a.district,
-    ci.city,
-    co.country
+	c.last_name,
+	a.address AS customer_address,
+    	a.district,
+    	ci.city,
+    	co.country
 FROM
 	customer c
 JOIN
 	address a
-    USING(address_id)
+    	USING(address_id)
 JOIN
 	city ci
-    USING(city_id)
+    	USING(city_id)
 JOIN
 	country co
-    USING(country_id)
+    	USING(country_id)
 ORDER BY
 	ci.city,
-    co.country
+    	co.country
